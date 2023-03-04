07bb625fe80fd96a78002f664a692a9ad141d8c8
Muhammar Ammar Masood <87846031+Muhammad-Ammar-Masood@users.noreply.github.com>
2023-03-04 09:29:54 +0500

    Update Real Estate.sol

--- Real Estate.sol
+++ Real Estate.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.7;
 
 contract realEstate{
-
-    event Sell(address seller, string location, uint sqFeet, uint price); 
-
+    
     struct Land {
         address seller;
         string location;
@@ -12,7 +10,7 @@ contract realEstate{
         uint price;
         bool sold;
     }
- 
+    
     mapping(uint => Land) public lands;
     uint public landNo;
 
@@ -25,6 +23,9 @@ contract realEstate{
     mapping(uint => Buyer) public buyers;
     uint public buyerNo;
 
+    event Sell(address seller, string location, uint sqFeet, uint price); 
+    event Buy(address buyer, Land landBrought, uint amountPaid); 
+ 
     function sellLand(string calldata location, uint sqFeet, uint price ) external {
         lands[landNo] = Land(msg.sender, location, sqFeet, price,false);
         ++landNo;
@@ -39,6 +40,7 @@ contract realEstate{
         lands[id].sold = true;
         buyers[buyerNo] = Buyer(msg.sender, lands[id], msg.value);
         ++buyerNo;
+         emit Buy(msg.sender, lands[id], msg.value);
     }
 
 }
